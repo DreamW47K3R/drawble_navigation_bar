d7b27077ee12b33d2de456cfda6f618d3e8103cb
DreamW47K3R <timur.rysbaev1@gmail.com>
2024-02-16 20:31:00 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "drawble_navigation_bar"
+include(":app")
+ 
